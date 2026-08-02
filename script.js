490a3e9d34ed8ca626d42fb0a1f4a76bbdd6fd5e
Ahtusk <ahteshamahmad3258@gmail.com>
2026-08-02 21:39:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginForm = document.querySelector("form");
+
+    loginForm.addEventListener("submit", function () {
+        alert("Login Successful!");
+    });
+
+});
